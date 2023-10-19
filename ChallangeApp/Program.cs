0d3f35b0cdb09293867ca9b386fa3f4cb41bd059
InventiveFuture <szymon.gme@gmail.com>
2023-10-19 19:45:23 +0200

    Zadanie domowe Dzień 4

--- ChallangeApp/Program.cs
+++ ChallangeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var Age = 33;
+string Name = "Ewa";
+var gender = "kobieta";
+
+
+if (Age < 40) 
+{
+    Console.WriteLine("Ewa, lat 33");
+
+}
+else if (Age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (Age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
