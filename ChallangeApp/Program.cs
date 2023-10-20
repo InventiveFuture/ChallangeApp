046e4f792170a165017da3d74c2708da8c046f8d
InventiveFuture <szymon.gme@gmail.com>
2023-10-20 11:32:51 +0200

    Dzień 4 - zadanie domowe

--- ChallangeApp/Program.cs
+++ ChallangeApp/Program.cs
@@ -1,19 +1,19 @@
 ﻿var Age = 33;
 string Name = "Ewa";
-var gender = "kobieta";
+var Gender = "Kobieta";
 
 
-if (Age < 40 && Name == "Ewa") 
+if (Age == 33 && Name == "Ewa") 
 {
     Console.WriteLine("Ewa, lat 33");
 
 }
-else if (Age < 30 && gender == "kobieta")
+else if (Age < 30 && Gender == "Kobieta")
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
-else if (Age < 18 && gender != "kobieta")
+else if (Age < 18 && Gender != "Kobieta")
 {
-    Console.WriteLine("Niepełnoletni mężczyzna");
+    Console.WriteLine("Niepełnoletni Mężczyzna");
 }
 
