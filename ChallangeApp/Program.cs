d7a5e09bd6dd4e1c41de21bb8211da91f6c3bdeb
InventiveFuture <szymon.gme@gmail.com>
2023-11-21 18:19:48 +0100

    Dzień 6

--- ChallangeApp/Program.cs
+++ ChallangeApp/Program.cs
@@ -1,69 +1,45 @@
-﻿int number = 459482;
-string numberAsString = number.ToString();
-char[] letters = numberAsString.ToArray();
+﻿using ChallangeApp;
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+Employee user1 = new Employee("Andrzej","Kaczmarek", 40);
+Employee user2 = new Employee("Paweł","Jagielski", 25);
+Employee user3 = new Employee("Agnieszka", "Kowalska", 31);
+Employee user4 = new Employee("Damian", "Nowak", 28);
 
-foreach (char letter in letters)
+user1.AddScore(10);
+user1.AddScore(30);
+user1.AddScore(40);
+
+user2.AddScore(50);
+user2.AddScore(5);
+user2.AddScore(10);
+
+user3.AddScore(30);
+user3.AddScore(15);
+
+user4.AddScore(30);
+
+List<Employee> employees = new List<Employee>()
+
 {
-    if (letter == '0')
+    user1, user2, user3, user4 
+};
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach (var employee in employees)
+{
+    if(employee.Result > maxResult)
     {
-        counter0++;
-    }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++;
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-    {
-        counter9++;
+        employeeWithMaxResult = employee;
+        maxResult= employee.Result;
     }
 }
 
-Console.WriteLine("0" + counter0);
-Console.WriteLine("1" + counter1);
-Console.WriteLine("2" + counter2);
-Console.WriteLine("3" + counter3);
-Console.WriteLine("4" + counter4);
-Console.WriteLine("5" + counter5);
-Console.WriteLine("6" + counter6);
-Console.WriteLine("7" + counter7);
-Console.WriteLine("8" + counter8);
-Console.WriteLine("9" + counter9);
+Console.WriteLine(employeeWithMaxResult.name + "" + employeeWithMaxResult.surname + "" + employeeWithMaxResult.age + ",wynik:" + employeeWithMaxResult.Result);
+
+
+
+
+
+
---
+++ ChallangeApp/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallangeApp
+{
+    public class Employee
+    {
+        private List<int> score = new List<int>();
+        public Employee(string name, string surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+           
+        }
+        public string name { get; private set; }
+        public string surname { get; private set; }
+        public string age { get; private set; }
+       
+
+
+        
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+
+        }
+
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+            
+        }
+    }
+}
+   
