5beef2b8153d0dd29cc658f8ce61cd2a30c57f95
InventiveFuture <szymon.gme@gmail.com>
2023-12-07 07:44:38 +0100

    Dzień 10

--- ChallangeApp/Employee.cs
+++ ChallangeApp/Employee.cs
@@ -2,7 +2,7 @@
 {
     public class Employee
     {
-        private List<float> grades= new List<float>();
+        private List<float> grades = new List<float>();
         public Employee(string name, string surname)
         {
             this.Name = name;
@@ -14,13 +14,50 @@
 
         public void AddGrade(float grade)
         {
-            this.grades.Add(grade);
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("invalid grade value");
+            }
         }
 
+        public void AddGrade(string grade)
+        {
+            if(float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else 
+            {
+                Console.WriteLine("String is not float");
+            }
+
+        }
+        public void AddGrade(long grade) 
+        {
+            float gradeAsFloat = (float)grade;
+            this.AddGrade(gradeAsFloat);        
+        }
+
+        public void AddGrade (int grade) 
+        {
+            float gradeAsFloat = grade;
+            this.AddGrade(gradeAsFloat);
+        }
+        
+        public void AddGrade (double grade) 
+        {
+            float gradeAsFloat = (float)grade;
+            this.AddGrade(gradeAsFloat);
+        
+        }
         public Statistics GetStatistics()
         {
             var statistics = new Statistics();
-            statistics.Average= 0;
+            statistics.Average = 0;
             statistics.MaxValue = float.MinValue;
             statistics.MinValue = float.MaxValue;
 
@@ -31,11 +68,9 @@
                 statistics.Average += grade;
             }
 
-                statistics.Average = statistics.Average / this.grades.Count;
+            statistics.Average = statistics.Average / this.grades.Count;
 
-
-                return statistics;
-            
+            return statistics;
         }
     }
 }
